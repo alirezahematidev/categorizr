e10bb5f783a87198eb8f934265e2a0887a98bfd1
alirezahematidev <alirezahematideveloper71@gmail.com>
2024-05-15 00:54:55 +0330

    release: react@0.0.10

--- scripts/prerelease-react.ts
+++ scripts/prerelease-react.ts
@@ -3,9 +3,9 @@ import fs from "node:fs/promises";
 import glob from "fast-glob";
 
 async function copy() {
-  const [dts] = await glob("dist/**/main.d.ts", { cwd: `packages/react`, absolute: true });
+  const files = await glob("**/dist/**/main.d.ts", { cwd: `packages/react`, absolute: true });
 
-  await fs.copyFile(dts, dts.replace(/\.d\.ts$/, ".d.cts"));
+  await Promise.all(files.map((dts) => fs.copyFile(dts, dts.replace(/\.d\.ts$/, ".d.cts"))));
 }
 
 copy();
